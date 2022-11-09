f2bd97606e73231157dca39cb8fbbc397c9328be
Jakub Kleban <jakub.kleban2000@gmail.com>
2022-11-09 21:45:13 +0100

    Add navigation options

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,21 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
-import SafeViewAndroid from "../components/SafeViewAndroid"
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import React from 'react';
+import SafeViewAndroid from "../components/SafeViewAndroid";
+import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-      <Text style={styles.text}>I am the HomeScreen</Text>
+    <SafeAreaView style={[SafeViewAndroid.AndroidSafeArea, tw`bg-white h-full`]}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{width: 100, height: 100, resizeMode: 'contain'}}
+          source={{uri: "https://links.papareact.com/gzs"}}
+        />
+        <NavOptions />
+      </View>
     </SafeAreaView>
   )
 }
 
 export default HomeScreen
-
-const styles = StyleSheet.create({
-    text: {
-        color: "green",
-    }
-})
